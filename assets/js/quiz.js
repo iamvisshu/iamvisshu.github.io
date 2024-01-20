d194ee86fff7b924a6e0d5a3ae967823201b41e0
Vishal Vishwakarma <vishwakarmavishal14@gmail.com>
2024-01-20 13:12:57 +0530

    Added quiz questions
    Added quiz questions

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -3,11 +3,21 @@ let currentQuestionIndex = 0;
 let score = 0;
 let timer;
 const questions = [
-    { question: 'Question 1: Test question one.', choices: ['True', 'False'], answer: 'True' },
-    { question: 'Question 2: Test question two.', choices: ['True', 'False'], answer: 'False' },
-    { question: 'Question 3: Test question three.', choices: ['A', 'B', 'C', 'D'], answer: 'A' },
-    { question: 'Question 4: Test question four.', choices: ['A', 'B', 'C', 'D'], answer: 'B' },
-    { question: 'Question 5: Test question five.', choices: ['A', 'B', 'C', 'D'], answer: ['A', 'B'] },
+    { question: 'Question 1: Java is a statically typed language.', choices: ['True', 'False'], answer: 'True' },
+    { question: 'Question 2: In Java, all classes inherit from the Object class.', choices: ['True', 'False'], answer: 'True' },
+    { question: 'Question 3: Java supports multiple inheritance.', choices: ['True', 'False'], answer: 'False' },
+    { question: 'Question 4: The == operator in Java compares the values of two objects.', choices: ['True', 'False'], answer: 'False' },
+    { question: 'Question 5: Java is platform independent.', choices: ['True', 'False'], answer: 'True' },
+
+    { question: 'Question 6: Who invented Java Programming?', choices: ['Guido van Rossum', 'James Gosling', 'Dennis Ritchie', 'Bjarne Stroustrup'], answer: 'James Gosling' },
+    { question: 'Question 7: Which component is used to compile, debug and execute the java programs?', choices: ['JRE', 'JIT', 'JDK', 'JVM'], answer: 'JDK' },
+    { question: 'Question 8: Which of the following is a type of polymorphism in Java Programming?', choices: ['Multiple polymorphism', 'Compile time polymorphism', 'Multilevel polymorphism', 'Execution time polymorphism'], answer: 'Compile time polymorphism' },
+    { question: 'Question 9: Which environment variable is used to set the java path?', choices: ['MAVEN_PATH', 'JAVA_PATH', 'JAVA', 'JAVA_HOME'], answer: 'JAVA_HOME' },
+    
+    { question: 'Question 10: Which of the following options lead to the portability and security of Java?', choices: ['A: Bytecode is executed by JVM', 'B: The applet makes the Java code secure and portable', 'C: Use of exception handling', 'D: Dynamic binding between objects'], answer: ['A: Bytecode is executed by JVM', 'B: The applet makes the Java code secure and portable'] },
+    { question: 'Question 11: Which of the following are valid declarations of a char in Java?', choices: ['A: char ch = \'\\utea\';', 'B: char ca = \'tea\';', 'C: char cr = \\u0223;', 'D: char cc = \'\\itea\';'], answer: ['A: char ch = \'\\utea\';', 'C: char cr = \\u0223;'] },
+    { question: 'Question 12: What does the expression float a = 35 / 0 return in Java?', choices: ['A: 0', 'B: Not a Number', 'C: Infinity', 'D: Run time exception'], answer: ['B: Not a Number', 'C: Infinity'] },
+
     // Add more questions as needed
     ];
 const startBtn = document.getElementById('start-btn');
@@ -77,7 +87,7 @@ function fullScreen() {
 
 // Initialize Quiz
 function startQuiz() {
-    let timeLeft = 2 * 60; // 2 minutes
+    let timeLeft = 5 * 60; // 2 minutes
     timer = setInterval(() => {
         if (timeLeft <= 0) {
             clearInterval(timer);
